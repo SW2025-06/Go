068964e46f82f52e3d1e590af9d1603eab5b1417
Tomida Masato <2311140075m@ed.fuk.kindai.ac.jp>
2025-11-19 08:11:26 +0000

    画像用のActive＿Storageを追加、Reviewとgame.rbにimageとの関連を追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
     has_many :reviews, dependent: :destroy
+    has_one_attached :image
     
     #validates :title, presence: true
     #validates :platform, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,7 @@ class Review < ApplicationRecord
   
   has_many :review_tags, dependent: :destroy
   has_many :tags, through: :review_tags
+  has_one_attached :image
 
   #validates :title, :body, :rating, presence: true
   #validates :rating, inclusion: { in: 1..5 }
