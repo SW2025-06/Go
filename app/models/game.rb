2ecdf1c1765fd4cab737fa9d51c99412ba3c48b9
Tomida Masato <2311140075m@ed.fuk.kindai.ac.jp>
2025-11-19 07:46:50 +0000

    モデル間の関連付けを行い、現時点で考えたバリデーションを追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+    
+    #validates :title, presence: true
+    #validates :platform, presence: true
+    #validates :genre, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
+  
+  has_many :review_tags, dependent: :destroy
+  has_many :tags, through: :review_tags
+
+  #validates :title, :body, :rating, presence: true
+  #validates :rating, inclusion: { in: 1..5 }
 end
