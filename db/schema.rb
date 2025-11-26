33f0bdb7510c33bfb90f583efd79bd9405598636
Tomida Masato <2311140075m@ed.fuk.kindai.ac.jp>
2025-11-26 08:34:57 +0000

    ユーザー関連のビューを編集してログイン/ログアウトのページが表示されるように

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_19_080823) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_26_072502) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_19_080823) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
