8c82ac850b28587968fe1eb42655d1edf71a378a
Tomida Masato <2311140075m@ed.fuk.kindai.ac.jp>
2025-12-03 06:12:52 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,8 @@ Rails.application.routes.draw do
   resources :tags
   resources :reviews
   resources :games
-<<<<<<< HEAD
-  root 'reviews#index'
-=======
   resources :users
   root "reviews#index"
->>>>>>> 4b059a7 (ユーザー関連のビューを編集してログイン/ログアウトのページが表示されるように)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
