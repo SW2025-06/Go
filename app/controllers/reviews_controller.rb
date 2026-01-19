03da23f57ab11f0f8ab56a9f1a446fca2a5935d4
Tomida Masato <2311140075m@ed.fuk.kindai.ac.jp>
2026-01-19 09:53:46 +0000

    Indexページのレイアウト等を改装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,46 +1,57 @@
+# app/controllers/reviews_controller.rb
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
     @reviews = Review.order(created_at: :desc)
-    @review = Review.new
+    @reviews = @reviews.where(genre: params[:genre]) if params[:genre].present?
+    @reviews = @reviews.where(platform: params[:platform]) if params[:platform].present?
+  end
+
+  def show
   end
 
   def new
     @review = Review.new
-    render partial: "form", locals: { review: @review }
-  end
-  
-  def destroy
-    @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_path, notice: "レビューを削除しました" }
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@review)) }
-    end
   end
 
   def create
     @review = current_user.reviews.build(review_params)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to reviews_path, notice: "レビューを作成しました" }
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.prepend("reviews_list", partial: "reviews/review", locals: { review: @review }),
-            turbo_stream.replace("new_review", partial: "reviews/new_link")
-          ]
-        end
-      else
-        format.html { render :index, status: :unprocessable_entity }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("new_review", partial: "reviews/form", locals: { review: @review }), status: :unprocessable_entity }
-      end
+    if @review.save
+      redirect_to @review, notice: "レビューを作成しました"
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    # 必要なら権限制御: redirect unless @review.user == current_user
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @review, notice: "レビューを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to reviews_path, notice: "レビューを削除しました"
   end
 
   private
 
+  def set_review
+    @review = Review.find_by(id: params[:id])
+    unless @review
+      redirect_to reviews_path, alert: "レビューが見つかりません"
+    end
+  end
+
   def review_params
-    params.require(:review).permit(:title, :body, :genre, :platform, :jacket, :rating, :purchase_url)
+    params.require(:review).permit(:title, :body, :genre, :platform, :jacket, :rating, :purchase_url, :game_id)
   end
 end
